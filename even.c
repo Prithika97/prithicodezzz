39ca6b256eb7e453a1d6c9eeee1c07d828a070f7
Prithika97 <30342546+Prithika97@users.noreply.github.com>
2017-07-23 14:55:38 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int counter;
+printf("even numbers between 1 to n\n");
+for(counter=1;counter<=n;counter++)
+{
+if(counter%2==0)
+printf("%d",counter);
+}
+}
+return 0;
+}
